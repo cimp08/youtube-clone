1cf131a0d8d528d340e6ed3efecacd457d022c16
Cemil Ülay <cemilulay@gmail.com>
2022-10-01 10:15:57 +0200

    add marginTop

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -3,9 +3,21 @@ import { CheckCircle } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 import { demoProfilePicture } from "../utils/constans";
 
-const ChannelCard = ({ channelDetail }) => {
+const ChannelCard = ({ channelDetail, marginTop }) => {
   return (
-    <Box sx={{ boxShadow: "none", borderRadius: "20px", display: "flex", justifyContent: "center", alignItems: "center", width: { xs: "356px", md: "320px"}, height: "326px", margin: "auto" }}>
+    <Box
+      sx={{
+        boxShadow: "none",
+        borderRadius: "20px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: { xs: "356px", md: "320px" },
+        height: "326px",
+        margin: "auto",
+        marginTop,
+      }}
+    >
       <Link to={`/channel/${channelDetail?.id?.channelId}`}>
         <CardContent
           sx={{
@@ -36,7 +48,10 @@ const ChannelCard = ({ channelDetail }) => {
           </Typography>
           {channelDetail?.statistics?.subscriberCount && (
             <Typography>
-              {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString()} Subscribers
+              {parseInt(
+                channelDetail?.statistics?.subscriberCount
+              ).toLocaleString()}{" "}
+              Subscribers
             </Typography>
           )}
         </CardContent>
