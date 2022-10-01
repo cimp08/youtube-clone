ac7784066caf2a876cb1828ae981233cb94aebb5
Cemil Ülay <cemilulay@gmail.com>
2022-10-01 10:14:36 +0200

    add new logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
-import { logo } from "../utils/constans";
 import { SearchBar } from "../components"
 
 
@@ -17,7 +16,7 @@ const Navbar = () => (
     }}
   >
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
-      <img src={logo} alt="logo" height={45} />
+      <img src="../utils/img/logo.png" alt="logo" height={45} />
     </Link>
     <SearchBar />
   </Stack>
